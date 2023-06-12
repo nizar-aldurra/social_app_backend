5981a5b25c7d1bcd70c6cf778393f8869d37b09f
Gillian Francisco <gillianfrancisco@gmail.com>
2023-06-12 18:50:17 +0400

    initial project

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    public function login(LoginRequest $request){
+        
+    }
+    public function register(RegisterRequest $request){
+        
+    }
+    public function logout(Request $request){
+        
+    }
+}
